fabf04acc8aceaa1fae3bedbd44fe79ab4811a2c
shankar Hampi <shankarshankar90877@gamil.com>
2025-08-05 16:45:12 +0530

    This file is operators.py

---
+++ operators.py
@@ -0,0 +1,62 @@
+# Arithmatic Operators
+# +, -, *, /, **, //, %
+
+
+
+# num1 = 1
+# num2 = 2
+
+# out = num1 + num2
+
+
+
+# # first_name = "Sachin"
+# # last_name = "Tendulkar"
+
+# # out = first_name - last_name
+
+
+
+# out1 = num1 / num2
+# # out2 = num1 // num2
+
+
+# # out2 = 10 ** 3 ==> 10 * 10 * 10
+
+# # print(out2)
+
+# out2 =  num1 % num2 # Modulus
+
+
+# print(out1)
+# print(out2)
+
+
+# lst1 = ['A', 'B', 'C']
+
+
+# lst2 = ['A', 'X', 'Y', 'Z', 'C']
+
+
+# out =  set(lst2) - set(lst1)
+
+# print(out)
+
+
+# count = 10
+
+# # count = count + 1
+
+# # count **= 100 ==> count = count - 100
+
+# # print(count)
+
+
+# print(count != 0)
+
+
+# A, B = True, False
+
+# print(not A)
+# print(B and A)
+# print(B or A)
