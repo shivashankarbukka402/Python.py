1086bc5acdd99958547b7b52df454f1262ba95c3
shankar Hampi <shankarshankar90877@gamil.com>
2025-08-05 16:33:40 +0530

    This file is conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,24 @@
+
+pass_status = True
+score = 25
+
+if pass_status:
+    if score < 60:
+        print("PASS")
+    elif score < 85:
+        print("FC")
+    else:
+        print("FCD")
+else:
+    print("Failed")
+    
+
+
+if a is b:
+        print("PASS")
+elif pass_status and score < 85:
+        print("FC")
+elif pass_status:
+        print("FCD")
+else:
+    print("Failed")
