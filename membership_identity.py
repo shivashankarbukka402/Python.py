a190403f326c959f61bdbf33f8d2a37dd62132c5
shankar Hampi <shankarshankar90877@gamil.com>
2025-08-05 16:50:02 +0530

    This file is Membership_identity.py

---
+++ membership_identity.py
@@ -0,0 +1,15 @@
+countries1 = ['UAE', 'BAN', 'USA', 'IND', 'PAK', 'AUS', "NZ", 'SA', 'SL']
+
+# countries2 = ['UAE', 'BAN', 'USA', 'IND', 'PAK', 'AUS', "NZ", 'SA', 'SL']
+
+# countries3 = countries1
+
+#Identity Operator
+# print(f"countries1 == countries2 ==> {countries1 == countries2} ")
+# print(f"countries1 == countries3 ==> {countries1 == countries3} ")
+# print(f"countries1 is countries2 ==> {countries1 is countries2} ")
+# print(f"countries1 is countries3 ==> {countries1 is countries3} ")
+
+
+#Membership Operator
+print("Best" in "Today is a Good Day")
