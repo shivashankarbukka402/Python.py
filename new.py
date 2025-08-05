2ae9be3f8babdff44fdf7cda2925d25d45d56b56
shankar Hampi <shankarshankar90877@gamil.com>
2025-08-05 17:02:31 +0530

    This file is new.py

---
+++ new.py
@@ -0,0 +1,58 @@
+# # num1 = 10
+# # num2 = 76576576.23783
+# # num3 = 10+20j
+
+
+# # print(num1, type(num1))
+
+# # print(num2, type(num2))
+
+# # print(num3, type(num3))
+
+
+# str1 = """It's a "Good" Day"""
+# str2 = "It's a \"Good\" Day"
+# str3 = 'It\'s a "Good" Day'
+
+# print("\n\n\n")
+# print("String1 ======> ", str1)
+# print("\n\n\n")
+# print("String2 ======> ", str2)
+# print("\n\n\n")
+# print("String3 ======> ", str3)
+# print("\n\n\n")
+
+
+str1 = "      The Socialist Unity Centre of India          "
+
+
+# length_of_string = len(country)
+
+# print("length_of_string ==> ", length_of_string)
+
+# print(country[length_of_string-3])
+
+# print(country[-3])
+
+
+# print(country[2])
+
+
+# S o c i a l i  s  t
+# 4 5 6 7 8 9 10 11 12
+
+
+
+name = "Sachin"
+age = 20
+
+text = "Hi, I am "+ name +" and I am " +" years old"
+
+# text = f"Hi, I am  {name}  and I am  {age} years old"
+
+
+# text = "Hi, I am  {}  and I am  {} years old".format(name, age)
+
+print(text)
+
+print("\n\n\n")
