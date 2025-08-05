95dabf0c97e06f127044892502a5394f821eb241
shankar Hampi <shankarshankar90877@gamil.com>
2025-08-05 16:31:33 +0530

    This file is list_tuples.py

---
+++ lists_tuples.py
@@ -0,0 +1,87 @@
+# lst1 = []
+# lst2 = list([])
+# lst3 = list("INDIA")
+# lst4 = "it defined".split()
+
+# lst5 = [None] * 5
+
+
+
+# print("List 1 ===>", lst1, type(lst1))
+# print("List 2 ===>", lst2, type(lst2))
+# print("List 3 ===>", lst3, type(lst3))
+# print("List 4 ===>", lst4, type(lst4))
+# print("List 5 ===>", lst5, type(lst5))
+
+
+# countries = ["UAE", "USA"]
+# print(countries)
+# countries.append("IND")
+# countries.append("PAK")
+# countries.append("AUS")
+# print(countries)
+
+
+# countries.insert(1, 'BAN')
+
+
+# print(countries, len(countries))
+
+
+# countries = ['UAE', 'BAN', 'USA', 'IND', 'PAK', 'AUS', "NZ", 'SA', 'SL']
+
+# print(countries)
+
+
+# countries.pop(2)
+
+
+# countries.remove('PAK')
+
+
+# print(countries)
+
+
+
+# countries = ['UAE', 'BAN', 'USA', 'IND', 'PAK', 'AUS', "NZ", 'SA', 'SL']
+# #countries.clear()
+
+# print(countries)
+
+
+
+
+
+# countries = []
+
+# print(countries)
+
+
+# country =  "INDIA"
+
+# country = list(country)
+
+# country.sort(reverse=True)
+
+
+
+# country = ''.join(country)
+
+# print(country)
+
+
+# todays_score = [1, 2, 4]
+
+# tuple1 = ("14", "15", "16", todays_score) 
+
+# todays_score = [23, 44]
+
+
+# print(tuple1)
+
+
+countries = {'UAE', 'BAN', 'USA', 'IND', 'PAK', 'AUS', "NZ"}
+
+countries_list = set1 - set2
+
+print(countries_list)
