eb04a2d01492314b67ed66c7b10c5c25fdf4f4f0
shankar Hampi <shankarshankar90877@gamil.com>
2025-08-05 16:52:18 +0530

    This file is First.py

---
+++ first.py
@@ -0,0 +1,17 @@
+Age = "40.34"
+name = 'Sachin'
+skill = ['Cricket', 'Studying']
+address = {
+	
+"City":"Delhi",
+    "Country":"India"
+}
+
+name = "sachin"             
+print(name)
+
+
+
+Comp1 = 20+10j
+
+x + iy
