13d07e640a10318244ef1539be9c0c4f63f8e840
shankar Hampi <shankarshankar90877@gamil.com>
2025-08-05 16:35:59 +0530

    This file is None_and_Boolean.py

---
+++ None_and_Boolean.py
@@ -0,0 +1,19 @@
+# # city = None
+# # print(city, type(city))
+
+
+
+# # isItRaining = False
+
+# something = -10
+
+# if something:
+#     print(f"{something} is truthy value")
+# else:
+#     print(f"{something} is falsy value")
+
+
+# nums = 10,20, None,
+
+
+# print(nums, type(nums), len(nums))
